ac4eab5d958c41149cc5caaba1e44f6f8785f024
Josh Kelley <joshkel@gmail.com>
2016-07-11 10:19:28 -0400

    Demos of controlled/uncontrolled, formatted/unformatted

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,12 +12,29 @@ var numberLocalizer = require('react-widgets/lib/localizers/simple-number');
 numberLocalizer();
 
 class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {unformatted: 0, formatted: 0};
+  }
   render() {
     return (
       <div className="index">
-        <NumberPicker/>
-        <NumberPicker format="-0.000"/>
-        <NumberPicker/>
+        <label>
+          Uncontrolled, unformatted
+          <NumberPicker/>
+        </label>
+        <label>
+          Uncontrolled, formatted
+          <NumberPicker format="-0.000"/>
+        </label>
+        <label>
+          Controlled, unformatted
+          <NumberPicker value={this.state.unformatted} onChange={(value) => this.setState({unformatted: +value})}/>
+        </label>
+        <label>
+          Controlled, formatted
+          <NumberPicker format="-0.000" value={this.state.formatted} onChange={(value) => this.setState({formatted: +value})}/>
+        </label>
       </div>
     );
   }
