5c6de940aa94b6f530d4c1338bfef951bc6c9bb9
Josh Kelley <joshkel@gmail.com>
2016-07-10 22:56:29 -0400

    Set up tab demo

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -45,8 +45,12 @@ function getDefaultModules() {
         loader: 'style-loader!css-loader!stylus-loader'
       },
       {
-        test: /\.(png|jpg|gif|woff|woff2)$/,
+        test: /\.(png|jpg|gif|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader?limit=8192'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       },
       {
         test: /\.(mp4|ogg|svg)$/,
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,22 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import {
+  NumberPicker
+} from 'react-widgets';
 
-let yeomanImage = require('../images/yeoman.png');
+require('react-widgets/lib/less/react-widgets.less');
+
+var numberLocalizer = require('react-widgets/lib/localizers/simple-number');
+numberLocalizer();
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <NumberPicker/>
+        <NumberPicker format="-0.000"/>
+        <NumberPicker/>
       </div>
     );
   }
